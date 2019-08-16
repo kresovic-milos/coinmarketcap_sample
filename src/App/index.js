ef0f9ab5ecc4bf71dceb125ccbe6af430fd9bf75
Miloš Kresović <kresovic.milos@gmail.com>
2019-08-16 14:58:31 +0200

    Align main screen

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import CCListingsLatest from './CCListingsLatest';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      <SafeAreaView style={styles.body}>
         <CCListingsLatest />
       </SafeAreaView>
     </>
   );
 };
 
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column'
+  }
+});
 export default App;
