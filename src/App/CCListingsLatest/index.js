0d1fc06a1f05e50d14b9d71cbc0ba3a8a5de385b
Miloš Kresović <kresovic.milos@gmail.com>
2019-08-16 15:28:42 +0200

    Remove recompose

--- src/App/CCListingsLatest/index.js
+++ src/App/CCListingsLatest/index.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-import { compose } from 'recompose';
 import CCListingsLatest from './CCListingsLatest';
 import { fetchCCListingsLatest } from './state/actions';
 import { getCCListingsLatest } from './state/selectors';
@@ -7,9 +6,7 @@ import { getCCListingsLatest } from './state/selectors';
 const mapStateToProps = state => ({ ...getCCListingsLatest(state) });
 const mapDispatchToProps = { fetchCCListingsLatest };
 
-export default compose(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
 )(CCListingsLatest);
