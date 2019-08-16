0b98b55bec477a9a8f52257a2777fb927e43807e
Miloš Kresović <kresovic.milos@gmail.com>
2019-08-16 14:58:00 +0200

    Limit response length

--- src/data/repositories/coinmarketcap/index.js
+++ src/data/repositories/coinmarketcap/index.js
@@ -2,7 +2,7 @@ import endpoints from '../../../utils/constants/endpoints';
 import Http from '../../Http';
 
 const getCryptocurrencyListingsLatest = async () =>
-  Http.get(`${endpoints.CC_LISTINGS_LATEST_ENDPOINT}`);
+  Http.get(`${endpoints.CC_LISTINGS_LATEST_ENDPOINT}?limit=10`);
 
 export default {
   getCryptocurrencyListingsLatest
